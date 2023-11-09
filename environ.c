dab3f1b6a7f4563db5089fb0eb7198e1aeccb616
Sote99 <Soteolayinka2@gmail.com>
2023-11-09 07:19:21 -0800

    Shell

---
+++ environ.c
@@ -0,0 +1,105 @@
+#include "shell.h"
+
+/**
+ * create_node - creats a new node
+ * @data: variable
+ * @number: keeps track of the variable
+ * Return: Pointer
+ */
+Node_env *create_node(char *data, int number)
+{
+	Node_env *new_node;
+
+	new_node = (Node_env *)malloc(sizeof(Node_env));
+	if (new_node == NULL)
+	{
+		perror("Error: malloc failed");
+		exit(EXIT_FAILURE);
+	}
+	new_node->data = data;
+	new_node->number = number;
+	new_node->next = NULL;
+
+	return (new_node);
+}
+
+/**
+ * add_to_list - adds node
+ * @head: head of the linked list
+ * @new_node: node to be added
+ */
+
+void add_to_list(Node_env **head, Node_env *new_node)
+{
+	Node_env *current;
+
+	if (*head != NULL)
+	{
+		current = *head;
+		while (current->next != NULL)
+			 = current->next;
+		current->next = new_node;
+	}
+	else
+		*head = new_node;
+}
+
+/**
+ * print_env - prints node
+ * @head: head of the linked list
+ * Return: 0
+ */
+
+void print_env(Node_env *head)
+{
+	Node_env *current;
+
+	current = head;
+	while (current != NULL)
+	{
+		write(STDOUT_FILENO, current->data, _strlen(current->data));
+		write(STDOUT_FILENO, "\n", 1);
+
+		current = current->next;
+	}
+}
+
+/**
+ * handle_env - handles the env
+ * Return: 0
+ */
+
+int handle_env(void)
+{
+	char **env = environ;
+	Node_env *head = NULL;
+
+	while (*env != NULL)
+	{
+		add_to_list(&head, create_node(*env, 0));
+		env++;
+	}
+	print_env(head);
+	free_env(head);
+	return (0);
+}
+
+/**
+ * free_env - frees list
+ * @head: head to the linked list
+ */
+
+void free_env(Node_env *head)
+{
+	Node_env *current = head;
+	Node_env *temp;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		if (temp->number == 1)
+			free(temp->data);
+		free(temp);
+	}
+}
